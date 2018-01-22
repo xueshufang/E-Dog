fa75aad8a587af737fc22babef83bac42689207e
xueshufang <1016656194@qq.com>
2018-01-22 20:21:47 +0800

    清理环境

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import 'antd-mobile/dist/antd-mobile.css';
-import './stylesheets/index.scss';
-
-import App from './javascripts/App';
-
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { Router, Route, IndexRedirect, hashHistory, Redirect } from 'react-router'
-
-import Home from './javascripts/components/home/Home'
-
-let routes = <Router history={hashHistory}>
-	<Route path="/" component={App}>
-		<IndexRedirect to="home"/>
-		<Route path="home" component={Home}/>
-		<Redirect from="*" to="home"/>
-	</Route>
-</Router>
-
-ReactDOM.render(
-	routes, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
