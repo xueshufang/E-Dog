562c76688f421714ffa11ccd097c976e07071b8a
xueshufang <1016656194@qq.com>
2018-01-23 12:01:27 +0800

    建文件

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//import axios from 'axios'
 class App extends Component {
+	
   render() {
     return (
       <div className="App">
