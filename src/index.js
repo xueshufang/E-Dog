0b72f4388a75362a405164429ffe1b6837b53214
xueshufang <1016656194@qq.com>
2018-01-23 17:04:49 +0800

    配置环境

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './stylesheets/index.scss';
+import App from './javascripts/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Router,Route,IndexRedirect,hashHistory,Redirect} from 'react-router'
+
+import GoodList from './javascripts/components/good-list/GoodList.js'
+
+let routes = <Router history={hashHistory}>
+	<Route path='/' component={App}>
+		<Route path="good-list" component={GoodList}/>
+	</Route>
+	
+</Router>
+ReactDOM.render(
+	routes
+	,document.getElementById('root'));
 registerServiceWorker();
---
+++ src/javascripts/components/good-list/GoodList.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+
+class GoodList extends Component {
+	constructor(props){
+		super(props)
+	}
+	
+	render(){
+		return(
+			<div>GoodList</div>
+		)
+	}
+}
+
+export default GoodList
